4bc5035d51c4df9c31d006ed15b584e3c2f68759
Everton Andre <evertonandre1996@gmail.com>
2022-07-25 11:39:13 -0300

    Math operations

--- rest-with-spring-boot-and-java-everton/src/main/java/br/com/everton/MathController.java
+++ rest-with-spring-boot-and-java-everton/src/main/java/br/com/everton/MathController.java
@@ -10,7 +10,7 @@ public class MathController {
 
 
     private final AtomicLong counter = new AtomicLong(); // VAI GERAR UM ID.
-
+//Soma
     //Recupera dados da URL
     @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method= RequestMethod.GET)
     public Double sum(
@@ -24,20 +24,7 @@ public class MathController {
 
      }
 
-    private Double convertToDouble(String strNumber) {
-        if(strNumber == null) return 0D;
-        String number = strNumber.replaceAll(",", ".");
-        if(isNumeric(number)) return Double.parseDouble(number);
-        return 0D;
-    }
-
-    private boolean isNumeric(String strNumber) {
-        if(strNumber == null) return false;
-        String number = strNumber.replaceAll(",", ".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
-
-    }
-
+//Subtração
 @RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}", method= RequestMethod.GET)
 public Double subtraction(
         @PathVariable(value = "numberOne") String numberOne,
@@ -50,6 +37,59 @@ public Double subtraction(
 
 }
 
+//Divisao
+    @RequestMapping(value = "/division/{numberOne}/{numberTwo}", method= RequestMethod.GET)
+    public Double division(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo) throws Exception { //PathVariable passa o valor diretamente na URL
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationsException("Please set a numeric value");
+        }
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+
+    }
+
+
+//media
+    @RequestMapping(value = "/average/{numberOne}/{numberTwo}", method= RequestMethod.GET)
+    public Double average(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo) throws Exception { //PathVariable passa o valor diretamente na URL
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationsException("Please set a numeric value");
+        }
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) /2;
+
+    }
+
+
+//Multiplicacao
+    @RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", method= RequestMethod.GET)
+    public Double multiplication(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo) throws Exception { //PathVariable passa o valor diretamente na URL
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationsException("Please set a numeric value");
+        }
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+
+    }
+
+//raiz quadrada
+    @RequestMapping(value = "/raiz/{numberOne}", method= RequestMethod.GET)
+    public Double raiz(
+            @PathVariable(value = "numberOne") String numberOne) throws Exception { //PathVariable passa o valor diretamente na URL
+        if(!isNumeric(numberOne)){
+            throw new UnsupportedMathOperationsException("Please set a numeric value");
+        }
+
+        return Math.sqrt(convertToDouble(numberOne));
+
+    }
+
     private Double convertToDouble(String strNumber) {
         if(strNumber == null) return 0D;
         String number = strNumber.replaceAll(",", ".");
@@ -63,4 +103,5 @@ public Double subtraction(
         return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 
     }
+
 }
---
+++ rest-with-spring-boot-and-java-everton/src/main/java/br/com/everton/NumberConverter.java
@@ -0,0 +1,4 @@
+package br.com.everton;
+
+public class NumberConverter {
+}
